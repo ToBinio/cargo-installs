0d2c2cb4d350f4e0cfc6060fe45762dc964eddbd
to_binio <tobias.frischmann1@gmail.com>
2023-12-20 09:54:21 +0100

    print with version

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -1,11 +1,33 @@
-use crate::util::crates::installed;
+use crate::util::crates::{installed, CrateData};
+use colored::Colorize;
 
 pub fn list() -> anyhow::Result<()> {
     let crates = installed()?;
 
-    for crate_data in crates {
-        println!("{}", crate_data.name);
-    }
+    print_crates(crates);
 
     Ok(())
+}
+
+pub fn print_crates(crates: Vec<CrateData>) {
+    let name_length = crates.iter().map(|data| data.name.len()).max().unwrap();
+    let version_length = crates.iter().map(|data| data.version.len()).max().unwrap();
+
+    println!(
+        "{:name_length$} {:version_length$}",
+        "Name".bold(),
+        "Version".bold(),
+        name_length = name_length,
+        version_length = version_length
+    );
+
+    for data in crates {
+        println!(
+            "{:name_length$} {:version_length$}",
+            data.name,
+            data.version,
+            name_length = name_length,
+            version_length = version_length
+        )
+    }
 }
