4099d0bcde62e79324c521ecfe663ba34390c759
to_binio <tobias.frischmann1@gmail.com>
2023-12-25 12:14:10 +0100

    sort crates

--- src/list/mod.rs
+++ src/list/mod.rs
@@ -1,5 +1,6 @@
 use crate::util::crates::{get_installed, CrateData};
 use colored::Colorize;
+use iter_tools::Itertools;
 
 pub fn list() -> anyhow::Result<()> {
     let crates = get_installed()?;
@@ -36,16 +37,20 @@ pub fn print_crates(crates: Vec<CrateData>) {
         "Latest".bold(),
     );
 
-    for data in crates {
-        let latest_version = if data.is_latest_version() {
-            data.latest_version.normal()
-        } else {
-            data.latest_version.red()
-        };
+    crates
+        .iter()
+        .sorted_by(|a, b| a.name.cmp(&b.name))
+        .sorted_by(|a, b| a.is_latest_version().cmp(&b.is_latest_version()))
+        .for_each(|data| {
+            let latest_version = if data.is_latest_version() {
+                data.latest_version.normal()
+            } else {
+                data.latest_version.red()
+            };
 
-        println!(
-            "{:name_length$} {:version_length$} {:newest_version_length$}",
-            data.name, data.version, latest_version
-        )
-    }
+            println!(
+                "{:name_length$} {:version_length$} {:newest_version_length$}",
+                data.name, data.version, latest_version
+            )
+        });
 }
