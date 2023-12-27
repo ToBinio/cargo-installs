6e9a58b5dd07e4e1ec63bf240312932e055df7cb
to_binio <tobias.frischmann1@gmail.com>
2023-12-27 15:10:48 +0100

    basic updating

--- src/update/mod.rs
+++ src/update/mod.rs
@@ -1,1 +1,20 @@
-pub fn update() {}
+use crate::util::crates::{get_installed, CrateData};
+use std::process::{Command, Stdio};
+
+pub fn update() -> anyhow::Result<()> {
+    let crates: Vec<CrateData> = get_installed()?
+        .into_iter()
+        .filter(|data| !data.is_latest_version())
+        .collect();
+
+    for data in crates {
+        let mut child = Command::new("cargo")
+            .arg("install")
+            .arg(data.name)
+            .spawn()?;
+
+        child.wait()?;
+    }
+
+    Ok(())
+}
