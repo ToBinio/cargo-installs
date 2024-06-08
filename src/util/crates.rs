26b2ec2c1d90cfe5f3c95da9467cea946d4bded0
tobinio <Tobias.frischmann1@gmail.com>
2024-06-08 08:51:25 +0200

    ignore local installs

--- src/util/crates.rs
+++ src/util/crates.rs
@@ -1,9 +1,12 @@
+use crate::util::crates::Origen::{Local, Remote};
 use crate::util::sparse::get_highest_version;
 use anyhow::anyhow;
+use clap::builder::Str;
+use colored::Colorize;
+use crates_index::Names;
 use home::cargo_home;
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::sync::mpsc::channel;
 use std::{fs, thread};
 
 pub fn get_installed() -> anyhow::Result<Vec<CrateData>> {
@@ -12,61 +15,82 @@ pub fn get_installed() -> anyhow::Result<Vec<CrateData>> {
     let file = fs::read_to_string(path)?;
     let parse: RawCrates = toml::from_str(&file)?;
 
-    //todo - simpler multithreading
-    let (sender, receiver) = channel::<Result<CrateData, anyhow::Error>>();
+    let mut handles = vec![];
 
     for (text, _bins) in parse.v1 {
-        let sender = sender.clone();
-        thread::spawn(move || {
-            let result = text.try_into();
-            sender.send(result).expect("something went wrong");
+        let handle = thread::spawn(move || {
+            return CrateData::from_definition(&text);
         });
+
+        handles.push(handle);
     }
 
-    drop(sender);
+    let mut crates = Vec::with_capacity(handles.len());
+    for handle in handles {
+        crates.push(
+            handle
+                .join()
+                .map_err(|_| anyhow!("could not join threads"))??,
+        );
+    }
 
-    Ok(receiver.iter().filter_map(|data| data.ok()).collect())
+    Ok(crates)
 }
 
 #[derive(Debug)]
 pub struct CrateData {
     pub name: String,
     pub version: String,
-    pub latest_version: String,
-    pub origen: String,
+    pub origen: Origen,
 }
 
 impl CrateData {
     pub fn is_latest_version(&self) -> bool {
-        self.version == self.latest_version
+        match &self.origen {
+            Local => true,
+            Remote { latest_version } => &self.version == latest_version,
+        }
     }
-}
 
-impl TryFrom<String> for CrateData {
-    type Error = anyhow::Error;
-
-    fn try_from(full_name: String) -> Result<Self, Self::Error> {
-        let mut split = full_name.split(' ');
+    pub fn from_definition(definition: &str) -> anyhow::Result<Self> {
+        let mut split = definition.split(' ');
 
         let name = split
             .next()
-            .ok_or(anyhow!("could not parse name: \"{}\")", full_name))?
+            .ok_or(anyhow!("could not parse name: \"{}\")", definition))?
             .to_string();
         let version = split
             .next()
-            .ok_or(anyhow!("could not parse version: \"{}\")", full_name))?
+            .ok_or(anyhow!("could not parse version: \"{}\")", definition))?
             .to_string();
         let origen = split
             .next()
-            .ok_or(anyhow!("could not parse origen: \"{}\")", full_name))?
+            .ok_or(anyhow!("could not parse origen: \"{}\")", definition))?
             .to_string();
+        let origen = Origen::from_definition(&name, &origen)?;
 
         Ok(CrateData {
-            latest_version: get_highest_version(&name)?,
             name,
             version,
             origen,
         })
+    }
+}
+
+#[derive(Debug)]
+pub enum Origen {
+    Local,
+    Remote { latest_version: String },
+}
+
+impl Origen {
+    pub fn from_definition(crate_name: &str, definition: &str) -> anyhow::Result<Self> {
+        if definition.starts_with("(path+") {
+            Ok(Local)
+        } else {
+            let latest_version = get_highest_version(crate_name)?;
+            Ok(Remote { latest_version })
+        }
     }
 }
 
