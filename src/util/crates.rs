0dbb8a9ada9393e65f28ad6522937bb2e3fbc2e2
tobinio <Tobias.frischmann1@gmail.com>
2024-06-08 07:52:54 +0200

    use .crates.toml instead of .crates2.json

--- src/util/crates.rs
+++ src/util/crates.rs
@@ -7,18 +7,18 @@ use std::sync::mpsc::channel;
 use std::{fs, thread};
 
 pub fn get_installed() -> anyhow::Result<Vec<CrateData>> {
-    let path = cargo_home()?.join(".crates2.json");
+    let path = cargo_home()?.join(".crates.toml");
 
     let file = fs::read_to_string(path)?;
+    let parse: RawCrates = toml::from_str(&file)?;
 
-    let parse: RawCrates = serde_json::from_str(&file)?;
-
+    //todo - simpler multithreading
     let (sender, receiver) = channel::<Result<CrateData, anyhow::Error>>();
 
-    for data in parse.installs {
+    for (text, _bins) in parse.v1 {
         let sender = sender.clone();
         thread::spawn(move || {
-            let result = data.try_into();
+            let result = text.try_into();
             sender.send(result).expect("something went wrong");
         });
     }
@@ -34,14 +34,6 @@ pub struct CrateData {
     pub version: String,
     pub latest_version: String,
     pub origen: String,
-    pub version_req: Option<String>,
-    pub bins: Vec<String>,
-    pub features: Vec<String>,
-    pub all_features: bool,
-    pub no_default_features: bool,
-    pub profile: String,
-    pub target: String,
-    pub rustc: String,
 }
 
 impl CrateData {
@@ -49,53 +41,38 @@ impl CrateData {
         self.version == self.latest_version
     }
 }
-impl TryFrom<(String, RawCrateData)> for CrateData {
+
+impl TryFrom<String> for CrateData {
     type Error = anyhow::Error;
 
-    fn try_from((full_name, data): (String, RawCrateData)) -> Result<Self, Self::Error> {
+    fn try_from(full_name: String) -> Result<Self, Self::Error> {
         let mut split = full_name.split(' ');
 
         let name = split
             .next()
             .ok_or(anyhow!("could not parse name: \"{}\")", full_name))?
             .to_string();
+        let version = split
+            .next()
+            .ok_or(anyhow!("could not parse version: \"{}\")", full_name))?
+            .to_string();
+        let origen = split
+            .next()
+            .ok_or(anyhow!("could not parse origen: \"{}\")", full_name))?
+            .to_string();
 
         Ok(CrateData {
             latest_version: get_highest_version(&name)?,
             name,
-            version: split
-                .next()
-                .ok_or(anyhow!("could not parse name: \"{}\")", full_name))?
-                .to_string(),
-            origen: split
-                .next()
-                .ok_or(anyhow!("could not parse name: \"{}\")", full_name))?
-                .to_string(),
-            version_req: data.version_req,
-            bins: data.bins,
-            features: data.features,
-            all_features: data.all_features,
-            no_default_features: data.no_default_features,
-            profile: data.profile,
-            target: data.target,
-            rustc: data.rustc,
+            version,
+            origen,
         })
     }
 }
 
 #[derive(Deserialize, Debug)]
 struct RawCrates {
-    pub installs: HashMap<String, RawCrateData>,
+    pub v1: RawCrateData,
 }
 
-#[derive(Deserialize, Debug)]
-struct RawCrateData {
-    pub version_req: Option<String>,
-    pub bins: Vec<String>,
-    pub features: Vec<String>,
-    pub all_features: bool,
-    pub no_default_features: bool,
-    pub profile: String,
-    pub target: String,
-    pub rustc: String,
-}
+type RawCrateData = HashMap<String, Vec<String>>;
--- src/util/table.rs
+++ src/util/table.rs
@@ -1,7 +1,7 @@
-pub fn get_column_width<T, F: Fn(&T) -> usize>(header: &str, data: &[T], supplier: F) -> usize {
+pub fn get_column_width<T, F: Fn(&T) -> usize>(header: &str, data: &[T], length_fn: F) -> usize {
     return data
         .iter()
-        .map(supplier)
+        .map(length_fn)
         .max()
         .unwrap_or(0)
         .max(header.len());
